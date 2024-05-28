fa95decfa930a008f91f27ad65fcd80d0e721ab1
catherinecheng02 <z5310517@ad.unsw.edu.au>
2024-05-28 11:44:12 +1000

    Create face.c

---
+++ tute01/face.c
@@ -0,0 +1,18 @@
+// face.c
+//
+// Written by Catherine Cheng (z5310517)
+// on 28-05-2024
+//
+// The program prints two simple faces
+//
+// Face 1:
+//  ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
